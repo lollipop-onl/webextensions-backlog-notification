a3bd75fcd94c4f1da2ae11fd0d241087e0d607ef
lollipop-onl <lollipop@simochee.net>
2021-07-11 12:36:10 +0900

    ✨ 無限スクロールローディングを実装

--- src/scripts/components/NotificationList.tsx
+++ src/scripts/components/NotificationList.tsx
@@ -1,17 +1,85 @@
-import React from 'react'
-import { NotificationItem } from './NotificationItem'
-import { useBacklogResource } from '~/hooks/backlog'
+import React, { useCallback, useEffect, useMemo, useRef } from 'react'
+import useSWR, { useSWRInfinite } from 'swr'
+import http from 'ky-universal'
+import { BacklogAPIEndpoints } from '~/api/types'
+import { NotificationItem } from '~/components/NotificationItem'
+import { getSpacesFromStorage } from '~/utils/webextension'
 
 export const NotificationList: React.VFC = () => {
-  const { data: notifications = [] } = useBacklogResource('/api/v2/notifications', {})
+  const reachingGuide = useRef<HTMLDivElement>(null);
+  const { data: spaces } = useSWR('webextension.storage.spaces', () => getSpacesFromStorage(), { suspense: true });
+  const { data: notificationPages = [], size, setSize } = useSWRInfinite<BacklogAPIEndpoints['get']['/api/v2/notifications']['response']>((pageIndex, previousPageData) => {
+    const space = spaces?.[0];
 
+    if (!space || (previousPageData?.length === 0)) {
+      return null;
+    }
+
+    const lastNotification = previousPageData?.slice().pop();
+
+    const searchParams = new URLSearchParams();
+
+    searchParams.set('count', '30');
+    searchParams.set('apiKey', space.apiKey);
+
+    if (lastNotification) {
+      searchParams.set('maxId', lastNotification.id.toString())
+    }
+
+    return `https://${space.domain}/api/v2/notifications?${searchParams.toString()}`
+  }, (url) => http(url).then((res) => res.json()), { suspense: true })
+
+  const isEmpty = useMemo(() => notificationPages[0]?.length === 0, [notificationPages])
+  const isReacingEnd = useMemo(() => isEmpty || notificationPages.slice().pop()?.length !== 30, [isEmpty, notificationPages])
+
+  const observer = useMemo(() => new IntersectionObserver((entities) => {
+    if (!notificationPages[size - 1]) {
+      return
+    }
+    
+    const isIntersecting = entities.some((entity) => entity.isIntersecting);
+
+    if (!isIntersecting) {
+      return
+    }
+    
+    setSize(size + 1);
+  }, {
+    threshold: 0.25,
+  }), [size, setSize, notificationPages])
+
+  useEffect(() => {
+    if (!reachingGuide.current) {
+      return;
+    }
+
+    observer.observe(reachingGuide.current);
+
+    return () => {
+      if (reachingGuide.current) {
+        observer.unobserve(reachingGuide.current);
+      }
+    }
+  }, [reachingGuide, observer])
+
+  useEffect(() => {
+    return () => {
+      observer.disconnect();
+    }
+  }, [observer]);
+  
   return (
-    <ul className="w-full">
-      {notifications.map((notification) => (
-        <li key={notification.id} className="border-b border-gray-400 last:border-none">
-          <NotificationItem notification={notification} />
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul className="w-full">
+        {notificationPages.flatMap((notifications) => notifications.map((notification, index) => (
+          <li key={notification.id} className="border-b border-gray-400 last:border-none">
+            <NotificationItem notification={notification} />
+          </li>
+        )))}
+      </ul>
+      { !isReacingEnd && notificationPages.length >= 1 && (
+        <div ref={reachingGuide} className="py-4 text-sm text-center text-gray-600">loading...</div>
+      ) }
+    </div>
   )
 }
--- src/scripts/components/PopupApp.tsx
+++ src/scripts/components/PopupApp.tsx
@@ -1,11 +1,21 @@
 import React, { Suspense } from "react";
 import { NotificationList } from "~/components/NotificationList";
+import backlogImage from '~~/images/backlog.png';
+
+const LoadingView: React.VFC = () => (
+  <div className="flex flex-col items-center justify-center py-20">
+    <div className="flex text-xl font-light text-gray-600">
+      <img className="h-8 mr-3" src={backlogImage} alt="Backlog" /> Notification Extension
+    </div>
+    <p className="mt-4 text-sm text-gray-400">by lollipop.onl</p>
+  </div>
+)
 
 export const PopupApp: React.VFC = () => {
   return (
     <div className="max-w-full mx-auto">
       <div className="max-w-full w-[480px]">
-        <Suspense fallback={<p>loading...</p>}>
+        <Suspense fallback={<LoadingView />}>
           <NotificationList />
         </Suspense>
       </div>
