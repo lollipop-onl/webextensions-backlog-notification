8133b70bbf4b50dd30c1c32d40b0a3aa39c652d0
simochee <lollipop@simochee.net>
2023-11-14 13:09:17 +0900

    Add Chrome manifest schema, update extension icon, and improve popup title and navbar

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -3,12 +3,14 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["src/**/*.{ts,tsx}", "*.html", "node_modules/ui/**/*.js"],
   theme: {
-    colors: {
-      brand: "#40cfa0",
-      priority: {
-        high: "#f42858",
-        medium: "#4488c5",
-        low: "#5eb5a6",
+    extend: {
+      colors: {
+        brand: "#40cfa0",
+        priority: {
+          high: "#f42858",
+          medium: "#4488c5",
+          low: "#5eb5a6",
+        },
       },
     },
   },
