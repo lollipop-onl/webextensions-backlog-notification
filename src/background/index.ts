6cf2669285f6d7201a9883b37f27c41df7cd7308
simochee <lollipop@simochee.net>
2023-11-14 14:38:51 +0900

    format code

--- src/background/index.ts
+++
@@ -1,5 +0,0 @@
-import Browser from 'webextension-polyfill';
-
-Browser.action.onClicked.addListener(() => {
-  Browser.browserAction.setBadgeText({ text: 'OK' });
-});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['src/**/*.{html,ts,tsx}'],
+  content: ['src/**/*.{ts,tsx}', '*.html'],
   theme: {
     extend: {},
   },
