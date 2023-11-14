adf3d8604bd0867a3e1d086c8706ff2a66e6ee12
simochee <lollipop@simochee.net>
2023-11-14 13:10:13 +0900

    cleanup repo

--- packages/extension/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { crx } from "@crxjs/vite-plugin";
-import tsconfigPaths from "vite-tsconfig-paths";
-import manifest from "./manifest.json" assert { type: "json" };
-
-export default defineConfig({
-  plugins: [tsconfigPaths(), react(), crx({ manifest })],
-});
--- packages/ui/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "tailwind-config";
-
-const config: Partial<Config> = {
-  presets: [sharedConfig],
-};
-
-export default config;
