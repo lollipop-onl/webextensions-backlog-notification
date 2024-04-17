a7e50459f7d86bb06bcd3dfb999789cd5dbe13c4
simochee <lollipop@simochee.net>
2024-04-17 19:30:10 +0900

    🛀 cleanup repo

--- README.md
+++
@@ -1,3 +0,0 @@
-# Backlog Ninja 🥷
-
-Backlog ([backlog.com](https://backlog.com/)) のお知らせや課題の更新通知をブラウザで隠密に受け取り、スピーディーにプロジェクトや課題に移動できる拡張機能です。
--- src/popup/entry.tsx
+++
@@ -1,4 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-const root = createRoot(document.getElementById('root')!);
-root.render(<div>hello world</div>);
