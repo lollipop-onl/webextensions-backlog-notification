d8357d00ac67d2c922efd4e63397de0ffcecf1b8
simochee <lollipop@simochee.net>
2025-01-02 18:04:39 +0900

    chore: upgrade tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,19 @@
-// @ts-check
-import lineClamp from '@tailwindcss/line-clamp';
-
+/** @type {import('tailwindcss').Config} */
 const config = {
-  mode: 'jit',
   content: ['./components/**/*.{html,tsx,ts}'],
-  darkMode: 'media',
   theme: {
     screens: {
-      sm: null,
       md: '480px',
-      lg: null,
-      xl: null,
-      '2xl': null,
     },
     extend: {
       colors: {
-        // @ts-expect-error
         backlog: '#40cfa0',
         'priority-high': '#f42858',
         'priority-medium': '#4488c5',
         'priority-low': '#5eb5a6',
-      },
-    },
-  },
-  plugins: [lineClamp],
-};
+      }
+    }
+  }
+}
 
 export default config;
